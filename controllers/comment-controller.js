30f9694bbb158c66c5f08a0d5fe5d25dc51bc5be
john.a.clark3 <john.a.clark3@gmail.com>
2022-04-11 16:19:24 -0600

    Hooked up the front end for the main page
    The main page now displays pizzas

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -3,6 +3,7 @@ const { Pizza, Comment } = require("../models");
 const commentController = {
   // add comment to pizza
   addComment({ params, body }, res) {
+    console.log(params);
     console.log(body);
     Comment.create(body)
       .then(({ _id }) => {
